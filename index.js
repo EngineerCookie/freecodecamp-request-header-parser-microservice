05c55587a45a31477bb7ed9c7fd44a94cf6ccec1
Tony <lam_273@hotmail.com>
2023-01-18 15:15:43 -0500

    script final

--- index.js
+++ index.js
@@ -19,12 +19,29 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', (req, res) => {
+  res.locals.obj = {"ipaddress" : undefined, "language": undefined, "software" : undefined};
+  res.locals.obj.ipaddress = req.ip;
+  res.locals.obj.language = req.headers['accept-language'];
+  res.locals.obj.software = req.headers['user-agent'];
+  res.json(res.locals.obj);
 });
+
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+
+/*
+- You should provide your own project, not the example URL.
+
+- A request to /api/whoami should return a JSON object with your IP address in the ipaddress key.
+
+- A request to /api/whoami should return a JSON object with your preferred language in the language key.
+
+- A request to /api/whoami should return a JSON object with your software in the software key.
+*/
